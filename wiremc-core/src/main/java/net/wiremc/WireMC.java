50a4260e43349f762bbb300e06f567cc3b6ab28e
GenerixDNS <Lightmicrash@gmail.com>
2023-03-09 11:49:57 +0100

    first commit

---
+++ wiremc-core/src/main/java/net/wiremc/WireMC.java
@@ -0,0 +1,17 @@
+package net.wiremc;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class WireMC extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
